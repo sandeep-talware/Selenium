31b6f7b6405830158f469bd785801a73ffdd09e7
SachinJoshi12687 <sachinjoshi12687@gmail.com>
2020-09-11 23:28:55 +0530

    added comments to validate the message in slack

--- Python/python/Login.py
+++ Python/python/Login.py
@@ -13,7 +13,7 @@ abs_path = os.path.abspath(os.path.join(os.path.abspath(os.path.join(os.path.dir
 #driver path is hard coded
 #driver = webdriver.Chrome("C:/Users/Sachin Joshi/PycharmProjects/Selenium/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
 
-#adding dynamic path to chromedriver
+#adding dynamic path to chromedriver so that  abybody can use this script without any changes
 driver = webdriver.Chrome(abs_path+"/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
 driver.get("https://www.saucedemo.com/index.html")
 driver.maximize_window()
