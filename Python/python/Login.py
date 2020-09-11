bf506e6317348522cc00b9e50cd2d3e2379aef51
SachinJoshi12687 <sachinjoshi12687@gmail.com>
2020-09-11 23:03:14 +0530

    added absolute path to the login script

--- Python/python/Login.py
+++ Python/python/Login.py
@@ -1,9 +1,20 @@
+import os
 import time
 
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome("C:/Users/Sachin Joshi/PycharmProjects/Selenium/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
+# this is the normal way to get the absolute path till the current folder
+#abs_path = os.path.abspath('.')
+#print(abs_path)
 
+#we need to go 2 level up to reach the parent folder so that we can navigate to the chromedriver folder
+abs_path = os.path.abspath(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),".."))
+#print(abs_path)
+
+#this path is hard coded
+#driver = webdriver.Chrome("C:/Users/Sachin Joshi/PycharmProjects/Selenium/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
+driver = webdriver.Chrome(abs_path+"/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
 driver.get("https://www.saucedemo.com/index.html")
 driver.maximize_window()
 
