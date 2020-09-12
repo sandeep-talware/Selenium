7ee1a327553e761645ccad92c2dfe6d42d72c941
SachinJoshi12687 <sachinjoshi12687@gmail.com>
2020-09-12 14:49:51 +0530

    added new method to read properties file, we need to extend this to return value to login method

--- Python/python/Login.py
+++ Python/python/Login.py
@@ -5,15 +5,15 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 
 # this is the normal way to get the absolute path till the current folder
-#abs_path = os.path.abspath('.')
+# abs_path = os.path.abspath('.')
 
-#we need to go 2 level up to reach the parent folder so that we can navigate to the chromedriver folder
+# we need to go 2 level up to reach the parent folder so that we can navigate to the chromedriver folder
 abs_path = os.path.abspath(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),".."))
 
-#driver path is hard coded
-#driver = webdriver.Chrome("C:/Users/Sachin Joshi/PycharmProjects/Selenium/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
+# driver path is hard coded
+# driver = webdriver.Chrome("C:/Users/Sachin Joshi/PycharmProjects/Selenium/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
 
-#adding dynamic path to chromedriver so that  abybody can use this script without any changes
+# adding dynamic path to chromedriver so that any body can use this script without any changes
 driver = webdriver.Chrome(abs_path+"/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
 driver.get("https://www.saucedemo.com/index.html")
 driver.maximize_window()
--- Python/python/__init__.py
+++ Python/python/__init__.py
@@ -1,1 +1,10 @@
+from jproperties import Properties
 print("hello world")
+
+# def _readproperties():
+configs = Properties()
+with open('C:/Users/Sachin Joshi/PycharmProjects/Selenium/Python/config/app-config.properties','rb') as config_file:
+    configs.load(config_file)
+    items_pro = configs.items()
+    for item in items_pro:
+        print(item[0],"=",item[1].data)
