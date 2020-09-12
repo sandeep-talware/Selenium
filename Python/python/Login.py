cc1fe49ab04895f58fe50840691cdc8b061af6be
SachinJoshi12687 <sachinjoshi12687@gmail.com>
2020-09-13 00:21:11 +0530

    added code as below, 1. read the properties file and return the dictionary to calling method 2. added method to read csv data using pandas library

--- Python/python/Login.py
+++ Python/python/Login.py
@@ -2,6 +2,8 @@ import os
 import time
 
 from selenium import webdriver
+
+from Python.python import _readproperties
 from selenium.webdriver.support import expected_conditions as EC
 
 # this is the normal way to get the absolute path till the current folder
@@ -10,12 +12,10 @@ from selenium.webdriver.support import expected_conditions as EC
 # we need to go 2 level up to reach the parent folder so that we can navigate to the chromedriver folder
 abs_path = os.path.abspath(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),".."))
 
-# driver path is hard coded
-# driver = webdriver.Chrome("C:/Users/Sachin Joshi/PycharmProjects/Selenium/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
-
-# adding dynamic path to chromedriver so that any body can use this script without any changes
+# apps variable will have the dictionary of all properties data in key:value pair
+apps = _readproperties(abs_path+'/Python/config/app-config.properties')
 driver = webdriver.Chrome(abs_path+"/Jar Files & Driver/chromedriver_win32_84.0.4147.30/chromedriver.exe")
-driver.get("https://www.saucedemo.com/index.html")
+driver.get(apps["APP_URL"])
 driver.maximize_window()
 
 driver.find_element_by_css_selector("input#user-name").send_keys("standard_user")
--- Python/python/__init__.py
+++ Python/python/__init__.py
@@ -1,10 +1,20 @@
-from jproperties import Properties
-print("hello world")
+import pandas
 
-# def _readproperties():
-configs = Properties()
-with open('C:/Users/Sachin Joshi/PycharmProjects/Selenium/Python/config/app-config.properties','rb') as config_file:
-    configs.load(config_file)
+from jproperties import Properties
+
+# This method will capture all the properties file data and return the dictionary object
+def _readproperties(filepath):
+    configs = Properties()
+    with open(filepath,'rb') as config_file:
+        configs.load(config_file)
     items_pro = configs.items()
+    prop_dict = {}
     for item in items_pro:
-        print(item[0],"=",item[1].data)
+        prop_dict[item[0]]=item[1].data
+    # print(item[0],"=",item[1].data)
+    return prop_dict
+
+# This method will read the csv data
+def _readcsvdata(filepath):
+    csvdata = pandas.read_csv(filepath)
+    return csvdata
